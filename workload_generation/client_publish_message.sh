3b4bdb45f454f5124409ed72c2861c5da98509a1
root <root@ip-172-31-13-0.eu-west-2.compute.internal>
2017-07-29 11:07:37 +0000

    ip addrss updated

--- workload_generation/client_publish_message.sh
+++ workload_generation/client_publish_message.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "client executed with id $1"
-mosquitto_pub -d -h 172.31.7.159 -t "topic/test" -m "msg number $1" -i "thing001" -q 1
+mosquitto_pub -d -h 172.31.15.244 -t "topic/test" -m "msg number $1" -i "thing001" -q 1
 
 
