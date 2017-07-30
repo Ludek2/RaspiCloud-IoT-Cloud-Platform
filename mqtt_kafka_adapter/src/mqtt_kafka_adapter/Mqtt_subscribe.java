b9f825743c1a4544f7f48f4a0ff5cf78e9d49b86
Ludek Navratil <ludek@Ludeks-MacBook-Pro.local>
2017-07-30 13:32:24 +0100

    mqtt_kafka_adapter started

---
+++ mqtt_kafka_adapter/src/mqtt_kafka_adapter/Mqtt_subscribe.java
@@ -0,0 +1,49 @@
+package mqtt_kafka_adapter;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class Mqtt_subscribe implements MqttCallback{
+
+		MqttClient client;
+
+		public Mqtt_subscribe() {
+		}
+
+		public void doDemo() {
+		    try {
+		        client = new MqttClient("tcp://35.177.223.189:1883", "Sending");
+		        client.connect();
+		        client.setCallback(this);
+		        client.subscribe("topic/test");
+		        //MqttMessage message = new MqttMessage();
+		        //message.setPayload("A single message from my computer fff"
+		        //        .getBytes());
+		        //client.publish("foo", message);
+		    } catch (MqttException e) {
+		        e.printStackTrace();
+		    }
+		}
+
+		@Override
+		public void connectionLost(Throwable cause) {
+		    // TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void messageArrived(String topic, MqttMessage message)
+		        throws Exception {
+		 System.out.println(message);   
+		}
+
+		@Override
+		public void deliveryComplete(IMqttDeliveryToken token) {
+		    // TODO Auto-generated method stub
+
+		}
+
+}
+
