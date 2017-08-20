fc7a7246a1211decea4026342b522ac455ee93c6
ludek_navratil <ludek@imaginecurve.com>
2017-08-20 11:52:33 +0100

    consumer microservice added

---
+++ microservices/microservice/microservice/Kafka_consumer.java
@@ -0,0 +1,18 @@
+package microservice;
+
+
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class Kafka_consumer implements Runnable {
+	public Kafka_consumer() {}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+}
