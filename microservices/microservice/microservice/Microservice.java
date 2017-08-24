547c35027814ea1eebae3098e38d91c4a7ce2714
ludek_navratil <ludek@imaginecurve.com>
2017-08-24 14:33:19 +0100

    database connection for microservice

--- microservices/microservice/microservice/Microservice.java
+++ microservices/microservice/microservice/Microservice.java
@@ -10,10 +10,22 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class Microservice {
-
+	static MongoClient mongoClient;
+	
 	public static void main(String[] args) {
+	   mongoClient = new MongoClient( "35.177.228.82" , 27017 );
+	   MongoDatabase db = mongoClient.getDatabase( "iot_test_db" );
+	   mongoClient.getReplicaSetStatus();
+	   //db.createCollection("test_collection");
+	   MongoCollection<Document> collection = db.getCollection("test_collection");
+       
 	   Properties consumerConfig = new Properties();
        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "35.176.144.206:9092");
        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "my-group");
@@ -28,11 +40,12 @@ public class Microservice {
            ConsumerRecords<byte[], byte[]> records = consumer.poll(1000);
            for (ConsumerRecord<byte[], byte[]> record : records) {
                System.out.printf("Received Message topic =%s, partition =%s, offset = %d, key = %s, value = %s\n", record.topic(), record.partition(), record.offset(), record.key(), record.value());
+               Document doc = new Document("receivedValue", record.value());
+         
+               collection.insertOne(doc);
            }
-
            consumer.commitSync();
        }
-
    }
 
    private static class  TestConsumerRebalanceListener implements ConsumerRebalanceListener {
@@ -45,7 +58,5 @@ public class Microservice {
        public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
            System.out.println("Called onPartitionsAssigned with partitions:" + partitions);
        }
-
 	}
-
 }
