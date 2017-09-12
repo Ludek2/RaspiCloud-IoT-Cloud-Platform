a793078229232ba52ba131969baf284aed75e083
Ubuntu <ubuntu@ip-172-31-13-0.eu-west-2.compute.internal>
2017-09-12 22:11:00 +0000

    date added to workload script

--- workload_generation/run_clients.sh
+++ workload_generation/run_clients.sh
@@ -17,7 +17,7 @@ else
 
     while IFS=, read col1 col2 col3 col4 col5 col6 col7 col8 col9 col10 col11
     do
-        $client_exec_file_path '{"stationId": "station1","date": "'$col1'","time": "'$col2'","maximum_wind_speed": "'$col3'","minimum_wind_speed": "'$col4'","mean_wind_speed": "'$col5'","wind_direction": "'$col6'","battery_voltage": "'$col7'","relative_humidity": "'$col8'","air_temperature": "'$col9'","net_solar_radiation": "'$col10'","rain_fall": "'$col11'"}'
+        $client_exec_file_path '{"stationId": "station1","date": "'$col1'","time": "'$col2'","maximum_wind_speed": "'$col3'","minimum_wind_speed": "'$col4'","mean_wind_speed": "'$col5'","wind_direction": "'$col6'","battery_voltage": "'$col7'","relative_humidity": "'$col8'","air_temperature": "'$col9'","net_solar_radiation": "'$col10'","rain_fall": "'$col11'","sent_from_iot_device_time": "'$(date +%s%N)'" }' 
        sleep 0.002
     done < dataSet.csv
 
